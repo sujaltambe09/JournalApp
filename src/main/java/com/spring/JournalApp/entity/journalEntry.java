56b3f29fb32fcd48ea317cb00a6ecce30ba848a7
Sujaltambe09 <sujal.tambe03@gmail.com>
2026-01-22 12:58:13 +0530

    Added ResponseEntity to get Http status code

--- src/main/java/com/spring/JournalApp/entity/journalEntry.java
+++ src/main/java/com/spring/JournalApp/entity/journalEntry.java
@@ -1,6 +1,9 @@
 package com.spring.JournalApp.entity;
 
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +13,7 @@ import java.util.Date;
 
 //POGO(Plain old java class)
 @Document
+@Data
 public class journalEntry {
 
     @Id
@@ -21,36 +25,5 @@ public class journalEntry {
 
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
